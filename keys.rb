77b47d308c39ca48304750cc1f37fba6dee72ab5
Chan Park <chan.park@life.church>
2020-09-04 14:28:20 -0500

    look up stock in back end

---
+++ keys.rb
@@ -0,0 +1,5 @@
+client = IEX::Api::Client.new(
+  publishable_token: 'Tpk_4e5e37f2823347b584aea2fb92612bd3',
+  secret_token: 'Tsk_baca4caabbc64960a0b267112a9c5df0',
+  endpoint: 'https://sandbox.iexapis.com/v1'
+)
