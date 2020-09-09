e2f399fc246947b43857e721811e65ad4725f0ce
Chan Park <chan.park@life.church>
2020-09-09 16:20:53 -0500

    put string

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,13 +1,20 @@
 class StocksController < ApplicationController
 
     def search
+        puts 'stock search 1'
         if params[:stock].present?
+            puts 'stock search 2'
+
             @stock = Stock.new_lookup(params[:stock].upcase)
             if @stock
+                puts 'stock search 3'
+
                 respond_to do |format|
                     format.js { render partial: 'users/result' }
                 end
-            else
+            else    
+                puts 'stock search 4'
+
                 respond_to do |format|
                     flash.now[:alert] = "Please enter a valid symbol to search"
                     format.js { render partial: 'users/result' }
